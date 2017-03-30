6b6c7355c2de5e37558f3435e4a9be4dd63018f0
athena3764 <jhhsu11@gmail.com>
2017-03-30 01:04:04 -0700

    Update change.php

--- public/change.php
+++ public/change.php
@@ -8,33 +8,28 @@
     {
         // else render form
         render("reset_form.php", ["title" => "Register"]);
-        
     }
 
     // else if user reached page via POST (as by submitting a form via POST)
-     
     if($_SERVER["REQUEST_METHOD"] == "POST"){
-
+        
+        //create a new password
         $new_password = password_hash($_POST["password"], PASSWORD_DEFAULT);
         
+        //check if the user enters username or password
         if (empty($_POST["username"]) || empty($_POST["password"])){
-            
             apologize("Please enter your username or password.");
         }
         
+        //check if the confirmation password is identical to the password
         if ($_POST["password"] !== $_POST["confirmation"]){
-            
-             apologize("Please re-enter your password.");
-            
+             apologize("Please re-enter your password.");   
         }
         
-        $reset = CS50::query("UPDATE users SET hash = ? WHERE username = ?", 
+        //update password
+        CS50::query("UPDATE users SET hash = ? WHERE username = ?", 
         $new_password, $_POST["username"] );
-        
-        if($reset == false){
-            
-            apologize("try again.");
-        }
+  
         
         redirect("login.php");
         
