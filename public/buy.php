da3ece0884fc1b49e0df83d595a31e380b784baf
athena3764 <jhhsu11@gmail.com>
2017-03-30 00:33:57 -0700

    Update buy.php

--- public/buy.php
+++ public/buy.php
@@ -1,75 +1,59 @@
 <?php
-    
-    // configuration
-    require("../includes/config.php");
-    
-        if ($_SERVER["REQUEST_METHOD"] == "GET")
-    {
+
+//configuration
+require("../includes/config.php");
+
+if ($_SERVER["REQUEST_METHOD"] == "GET"){
         //render form
         render("buy_form.php", ["title" => "Buy"]);
     }
-
+if ($_SERVER["REQUEST_METHOD"] == "POST"){
     
-        if ($_SERVER["REQUEST_METHOD"] == "POST")
+    //check if a stock symbol or share is entered.
+    if (empty($_POST["symbol"]) || empty($_POST["shares"]))
     {
-        
-        if (empty($_POST["symbol"]) || empty($_POST["shares"]))
-        {
-            apologize("You must provide a stock symbol or share.");
-        }
-        
-        if (!preg_match("/^\d+$/", $_POST["shares"]))
+        apologize("You must provide a stock symbol or share.");
+    }
+    //check if the input is a non-negative digit number
+    if (!preg_match("/^\d+$/", $_POST["shares"]))
     {
         apologize("Please enter a non-negative integer for shares.");
     }
-       
-        //Returns a stock by symbol 
-        $stock = lookup($_POST["symbol"]);
-         
-        if($stock == false){
-            
-            apologize("please enter a valid symbol.");
-            
-        }
-        
-        
-       $user_cash = CS50::query("SELECT cash FROM users WHERE id = ?", $_SESSION["id"]);
-       
-      if ($user_cash[0]["cash"] - ($stock["price"] * $_POST["shares"]) <0){
-            
-           apologize("Not enough cash.");
-          
-          
-      }
-      else{
-          
-           $transaction = 'BUY';
-          
-          $buy = CS50::query("INSERT INTO history (id, transaction,  symbol, shares, date_time) 
-VALUES(?, ?, ?, ?, NOW())", $_SESSION["id"], $transaction,
-strtoupper($stock["symbol"]), $_POST["shares"]);
-          
-          
-          $add = CS50::query("INSERT INTO portfolio (id, symbol, shares) 
-VALUES(?, ?, ?) ON DUPLICATE KEY UPDATE shares = shares + VALUES(shares)", $_SESSION["id"], 
-strtoupper($stock["symbol"]), $_POST["shares"]);
-
-if ($add == false){
     
-    apologize("Try to add again.");
+    //Returns a stock by symbol 
+    $stock = lookup($_POST["symbol"]);
     
-}
-
- CS50::query("UPDATE users SET cash = cash - ? WHERE id = ?", 
-        ($stock["price"] * $_POST["shares"]), $_SESSION["id"]);
-      
-        redirect("index.php");
-      
-          
-          
-      }
+    //check if the user enters the symbol.
+    if($stock == false){
+        apologize("please enter a valid symbol.");
     }
     
+    //retrieve user's cash from the database
+    $user_cash = CS50::query("SELECT cash FROM users WHERE id = ?", $_SESSION["id"]);
+    
+    //check if user has enough cash
+    if ($user_cash[0]["cash"] - ($stock["price"] * $_POST["shares"]) <0){
+        apologize("Not enough cash.");
+    }
+    else{
+        $transaction = 'BUY';
+        
+        //insert the new records in the database
+        $buy = CS50::query("INSERT INTO history (id, transaction,  symbol, shares, date_time) 
+VALUES(?, ?, ?, ?, NOW())", $_SESSION["id"], $transaction,
+strtoupper($stock["symbol"]), $_POST["shares"]);
+        $add = CS50::query("INSERT INTO portfolio (id, symbol, shares) 
+VALUES(?, ?, ?) ON DUPLICATE KEY UPDATE shares = shares + VALUES(shares)", $_SESSION["id"], 
+strtoupper($stock["symbol"]), $_POST["shares"]);
+        
+        //update cash
+        CS50::query("UPDATE users SET cash = cash - ? WHERE id = ?",
+        ($stock["price"] * $_POST["shares"]), $_SESSION["id"]);
+        
+        //redirect the user to another page.
+        redirect("index.php"); 
+      }
+    } 
 ?>
  
 
