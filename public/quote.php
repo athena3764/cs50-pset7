2d3ac1ee1bb7fc32d7645e02f76fb39a6274bb4b
athena3764 <jhhsu11@gmail.com>
2017-03-30 16:32:45 -0700

    Update quote.php

--- public/quote.php
+++ public/quote.php
@@ -11,21 +11,23 @@
  
     // else if user reached page via POST (as by submitting a form via POST)
     if ($_SERVER["REQUEST_METHOD"] == "POST")
-    {
+    {   
+        //check if user enters a stock symbol
         if (empty($_POST["quote"]))
         {
             apologize("You must provide a stock symbol.");
         }
-        
-         $stock = lookup($_POST["quote"]);
          
+        //return a stock by symbol
+        $stock = lookup($_POST["quote"]);
+        
+        //check if stock symbol is valid
         if($stock == false){
-            
             apologize("please enter a valid symbol.");
-            
         }
         else{
             
+            //use the render function to display the record via quote.php
             render("quote.php",  ["s" => $stock]);
 
         }
