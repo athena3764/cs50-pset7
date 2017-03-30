8a866f1bbfaa45ab96d3f4451798e399b5e6226e
athena3764 <jhhsu11@gmail.com>
2017-03-30 01:18:16 -0700

    Update history.php

--- public/history.php
+++ public/history.php
@@ -1,28 +1,28 @@
 <?php
 
     // configuration
-    require("../includes/config.php"); 
- 
-   $rows = CS50::query("SELECT transaction, date_time, symbol, shares 
-   FROM history WHERE id = ?", $_SESSION["id"]);
-   
-   $positions = [];
-   
-   foreach($rows as $row){
-       
-       $stock = lookup($row["symbol"]);
-        if ($stock !== false)
-    {
-       $positions[]=[
-           "transaction" => $row["transaction"],
-           "date_time" => $row["date_time"],
-           "symbol" => $stock["symbol"],
-           "shares" => $row["shares"],
-           "price" => $stock["price"]
-           ];
-    }
-   }
+    require("../includes/config.php");
     
-    render("history_form.php", ["positions" => $positions, "title" => "History"]);
+    //returns a result set of records from the database
+    $rows = CS50::query("SELECT transaction, date_time, symbol, shares 
+    FROM history WHERE id = ?", $_SESSION["id"]);
+   
+    $positions = [];
+
+    //stores each record in an array
+    foreach($rows as $row){
+        $stock = lookup($row["symbol"]);
+        if ($stock !== false){
+            $positions[]=[
+                "transaction" => $row["transaction"],
+                "date_time" => $row["date_time"],
+                "symbol" => $stock["symbol"],
+                "shares" => $row["shares"],
+                "price" => $stock["price"]
+            ];
+        }
+    }
+// use the render function to display the record via history_form.php 
+render("history_form.php", ["positions" => $positions, "title" => "History"]);
 
 ?>
