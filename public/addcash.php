20e4edec2938d3322d72707ff12d10f469c55272
athena3764 <jhhsu11@gmail.com>
2017-03-30 00:46:36 -0700

    Update addcash.php

--- public/addcash.php
+++ public/addcash.php
@@ -9,12 +9,17 @@ if ($_SERVER["REQUEST_METHOD"] == "GET"){
     render("addcash_form.php", ["title" => "Add Cash"]);
 }
 if ($_SERVER["REQUEST_METHOD"] == "POST"){
+    //check if the user enters the number.
     if (empty($_POST["money"])){
         apologize("You must enter a number.");
     }
+    
+    //check if the input is a non-negative digit number
     if (!preg_match("/^\d+$/", $_POST["money"])){
         apologize("Please enter a non-negative integer for cash.");
     }
+    
+    //update cash
     CS50::query("UPDATE users SET cash = cash + ? WHERE id = ?",
                 $_POST["money"], $_SESSION["id"]);
     redirect("index.php");
