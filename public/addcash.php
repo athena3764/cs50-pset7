73dc7d0cb1997aeaf6f19aaa6115b4b20030d16b
athena3764 <jhhsu11@gmail.com>
2017-03-29 23:08:09 -0700

    Update addcash.php

--- public/addcash.php
+++ public/addcash.php
@@ -3,30 +3,21 @@
     // configuration
     require("../includes/config.php");
     
-        if ($_SERVER["REQUEST_METHOD"] == "GET")
-    {
-        //render form
-        render("addcash_form.php", ["title" => "Add Cash"]);
-    }
+if ($_SERVER["REQUEST_METHOD"] == "GET"){
     
-            if ($_SERVER["REQUEST_METHOD"] == "POST")
-    {
-        
-         if (empty($_POST["money"]))
-        {
-            apologize("You must enter a number.");
-        }
-        
-             if (!preg_match("/^\d+$/", $_POST["money"]))
-    {
+    //render form
+    render("addcash_form.php", ["title" => "Add Cash"]);
+}
+if ($_SERVER["REQUEST_METHOD"] == "POST"){
+    if (empty($_POST["money"])){
+        apologize("You must enter a number.");
+    }
+    if (!preg_match("/^\d+$/", $_POST["money"])){
         apologize("Please enter a non-negative integer for cash.");
     }
-    
-    CS50::query("UPDATE users SET cash = cash + ? WHERE id = ?", 
-        $_POST["money"], $_SESSION["id"]);
-        
-        redirect("index.php");
-        
-    }
-    
-    ?>
+    CS50::query("UPDATE users SET cash = cash + ? WHERE id = ?",
+                $_POST["money"], $_SESSION["id"]);
+    redirect("index.php");
+}
+
+?>
