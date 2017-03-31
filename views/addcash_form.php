fb0a4e376d42f224787360f2ae82e293099d1cc0
athena3764 <jhhsu11@gmail.com>
2017-03-31 00:10:35 -0700

    Update addcash_form.php

--- views/addcash_form.php
+++ views/addcash_form.php
@@ -3,7 +3,6 @@
         <div class="form-group">
             <input autofocus class="form-control" name="money" placeholder="Cash amount" type="number"/>
         </div>
-        
             <button class="btn btn-default" type="submit">Add Cash</button>
         </div>
     </fieldset>
